b3dc90aeeedf2c34dcec7bf9a5228046dc9fcbb8
Ivan Evdokimov <dr.evdokimov@gmail.com>
2023-08-30 11:48:20 +0100

    Final commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,22 @@
+import ast
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(override=True)
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY_TASKI')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG_TASKI')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS_LIST = os.getenv('ALLOWED_HOSTS_TASKI')
+
+if ALLOWED_HOSTS_LIST:
+    ALLOWED_HOSTS = ast.literal_eval(ALLOWED_HOSTS_LIST)
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
